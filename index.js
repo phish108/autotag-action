9f3bef77b1aeba893ba271d10dfccd7541051ab3
Christian Glahn <cpglahn@gmail.com>
2020-03-14 19:34:32 +0100

    fix typos

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ try {
     const nameToGreet = core.getInput('dry-run');
     console.log(`Hello ${nameToGreet}!`);
     const time = (new Date()).toTimeString();
-    core.setOutput("new-tag", "hello world);
+    core.setOutput("new-tag", "hello world");
     core.setOutput("tag", time);
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2)
     console.log(`The event payload: ${payload}`);
-    
+
     const payload2 = JSON.stringify(github.context.github, undefined, 2)
     console.log(`The event payload: ${payload2}`);
 
