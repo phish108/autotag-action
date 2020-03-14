35b1477d104d2532daad1cf0fd72216cb459de6e
Christian Glahn <cpglahn@gmail.com>
2020-03-14 20:10:46 +0100

    make release ready

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ const github = require('@actions/github');
 try {
     // `who-to-greet` input defined in action metadata file
     const nameToGreet = core.getInput('dry-run');
+    
     console.log(`Hello ${nameToGreet}!`);
+    
     const time = (new Date()).toTimeString();
+
     core.setOutput("new-tag", "hello world");
     core.setOutput("tag", time);
+
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2)
     console.log(`The event payload: ${payload}`);
@@ -15,6 +19,7 @@ try {
     const payload2 = JSON.stringify(github.context.github, undefined, 2)
     console.log(`The event payload: ${payload2}`);
 
-  } catch (error) {
+  } 
+  catch (error) {
     core.setFailed(error.message);
   }
