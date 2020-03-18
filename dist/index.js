8fb8719b84a9e2234f02e108d53d38782a95c235
GitHub Action <action@github.com>
2020-03-18 20:48:16 +0000

    updated dist file for ebf433930b391dab2e491733194999e1982991d6

--- dist/index.js
+++ dist/index.js
@@ -2299,97 +2299,104 @@ async function getLatestTag(octokit, repository) {
 }
 
 async function loadBranch(octokit, branch) {
-    const result = await octokit.git.listMatchingRefs({
+    const result = octokit.git.listMatchingRefs({
         owner: github.context.payload.repository.owner.name,
         repo: github.context.payload.repository.name,
         ref: `heads/${branch}`
     });
-    
+
     return result.data.shift();
 }
 
 async function action() {
-    // Inputs
+    // prepare octokit
     const token = core.getInput('github-token');
     const octokit = new github.GitHub(token);
     
-    const dryRun = core.getInput('dry-run').toLowerCase();
-    const level = core.getInput('bump');
-    const forceBranch = core.getInput('branch');
+    // load inputs
+    // const customTag     = core.getInput('custom-tag');
+    const dryRun        = core.getInput('dry-run').toLowerCase();
+    const level         = core.getInput('bump');
+    const forceBranch   = core.getInput('branch');
     const releaseBranch = core.getInput('release-branch');
+    const withV         = core.getInput('with-v').toLowerCase() === "false" ? "" : "v";
 
-    const withV = core.getInput('with-v').toLowerCase() === "false" ? "" : "v";
+    let branchInfo;
 
     if (forceBranch) {
-        console.log(`verify that ${ forceBranch }-branch exists`);
-        // verify that the brnach exists.
-        const okBranch = await loadBranch( octokit, forceBranch);
+        console.log(`check forced branch ${forceBranch}`);
 
-        if (okBranch) {
-            console.log("branch confirmed, continue");
+        branchInfo = await loadBranch(octokit, forceBranch);
+
+        if (!branchInfo) {
+            throw new Error("unknown branch provided");       
         }
-        else {
-            throw new Error("unknown branch provided");
-        }
+    
+        console.log("branch confirmed, continue");    
     }
 
-    const sha = github.context.sha;
+    if (!branchInfo) {
+        const activeBranch = github.context.ref.split("/").pop();
 
-    // release branchs other than master
-    const curBranch = forceBranch || github.context.ref.split("/").pop() ;
-
-    console.log(curBranch + " is used" );
-
-    // if force branch
-    console.log("fetch matching heads");
-
-    const branchInfo  = await loadBranch(octokit, curBranch);
-    const curHeadSHA = branchInfo.object.sha;
+        console.log(`load the history of activity-branch ${ activeBranch }`);
+        branchInfo  = await loadBranch(octokit, activeBranch);
+    }
     
-    console.log(`maching refs: ${ JSON.stringify(curHeadSHA, undefined, 2) }`);
+    // the tag for tagging
+    const sha = branchInfo.object.sha;
+
+    console.log(`maching refs: ${ sha }`);
 
     const latestTag = await getLatestTag(octokit, github.context.payload.repository);
 
+    console.log(`the previous tag of the repository ${latestTag}`);
+
     core.setOutput("tag", latestTag.name);
 
-    if (latestTag.commit.sha === github.context.sha) {
-        console.log("no new commits, avoid tagging");
-
-        // core.setOutput("new-tag", latestTag.name);
-        return;
+    if (latestTag.commit.sha === sha) {
+        throw new Error("no new commits, avoid tagging");
     }
 
     console.log(`The repo tags: ${ JSON.stringify(latestTag, undefined, 2) }`);
 
     const version   = semver.clean(latestTag.name);
-    let nextVersion = semver.inc(version, level);
-
-    console.log(`current branch is ${curBranch}`);
-
-    if (curBranch !== releaseBranch) {
-        console.log("not a release branch, create a prerelease")
-        nextVersion = semver.inc(version, 
-                                 "pre"+level, 
-                                 github.context.sha.slice(0, 6)
-        );
+    let nextVersion = semver.inc(
+        version, 
+        "pre" + level, 
+        github.context.sha.slice(0, 6)
+    );
+    
+    // check if the current branch is actually a release branch
+    const branchName = branchInfo.ref.split("/").pop();
+    
+    for (const branch of releaseBranch.split(",")) {
+        const testBranchName = new RegEx(branch);
+        if (testBranchName.test(branchName)) {
+            console.log(`${ branchName } is a release branch`);
+            nextVersion = semver.inc(version, level);
+            break;
+        }
     }
 
     console.log( `bump tag ${ nextVersion }` );
 
     core.setOutput("new-tag", nextVersion);
 
-    if (dryRun === "false") {
-        console.log(`add tag ${nextVersion} to ${ curBranch }`);
-    
-        const ref = `refs/tags/${withV}${nextVersion}`;
-
-        const result = await octokit.git.createRef({
-            owner: github.context.payload.repository.owner.name,
-            repo: github.context.payload.repository.name,
-            ref,
-            sha
-        });
+    if (dryRun === "true") {
+        console.log("dry run, don't perform tagging");
+        return
     }
+
+    console.log(`really add tag ${ withV }${ nextVersion }`);
+
+    const ref = `refs/tags/${ withV }${ nextVersion }`;
+
+    const result = await octokit.git.createRef({
+        owner: github.context.payload.repository.owner.name,
+        repo: github.context.payload.repository.name,
+        ref,
+        sha
+    });
 }
 
 action()
